a70ced64c4d437fe18500f8e683c54b246bd2116
Hilman_Amr <hilmanamr22@gmail.com>
2022-10-30 21:10:58 +0700

    File pertama

---
+++ python2.py
@@ -0,0 +1,13 @@
+nama = input("Masukkan nama kamu:")
+umur = input("Masukkan umur kamu:")
+
+print("Hello", nama,"umur kamu adalah",umur,"tahun")
+
+import math
+r = float(input("Masukan Jari-jari : "))
+
+luas = math.pi*(r*r)
+keliling = 2*math.pi*r
+
+print ("Luas Lingkaran \t\t= ",luas)
+print ("Keliling Lingkaran\t= ",keliling)
